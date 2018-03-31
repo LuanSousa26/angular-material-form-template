54dada3ab6d3b55c155169c1718a934af24bebdf
unknown <luan26@uninove.edu.br>
2018-03-31 19:30:51 -0300

    PROJTEMP01-Correção nos campos CPF e CEP

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -11,9 +11,12 @@ import {
 import {
   EnderecoModel
 } from '../models/enderecoModel';
-import { Observable } from 'rxjs/Observable';
-import { HttpResponse } from '@angular/common/http';
-
+import {
+  Observable
+} from 'rxjs/Observable';
+import {
+  HttpResponse
+} from '@angular/common/http';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
@@ -31,46 +34,46 @@ export class CadastroComponent {
   constructor(private cadastroService: CadastroService) {
       this.usuario.endereco = new EnderecoModel();
   }
-
   salvarDados() {
-    this.cadastroService.salvarUsuario(this.usuario).subscribe(resposta => {
-      this.postRetorno = resposta;
-      console.log(resposta);
-    });
-    alert('Usuario Salvo!');
+      this.cadastroService.salvarUsuario(this.usuario).subscribe(resposta => {
+          this.postRetorno = resposta;
+          console.log(resposta);
+      });
+      alert('Usuario Salvo!');
   }
-
   verificarCep() {
-      if (this.usuario.endereco.cep !== undefined && this.usuario.endereco.cep.includes('-')) {
-          const cepV = /\-/gi;
-          this.usuario.endereco.cep = this.usuario.endereco.cep.replace(cepV, '');
-      }
-      if (this.usuario.endereco.cep.length > 7) {
-          this.cadastroService
-              .getCep(this.usuario.endereco.cep)
-              .subscribe((endereco) => {
-                  this.usuario.endereco = endereco;
-              });
-
+      if (this.usuario.endereco.cep !== '') {
+          if (this.usuario.endereco.cep.includes('-')) {
+              const cepV = /\-/gi;
+              this.usuario.endereco.cep = this.usuario.endereco.cep.replace(cepV, '');
+          }
+          if (this.usuario.endereco.cep.length > 7) {
+              this.cadastroService
+                  .getCep(this.usuario.endereco.cep)
+                  .subscribe((endereco) => {
+                      this.usuario.endereco = endereco;
+                  });
+          }
       }
   }
   verificarCpf() {
       this.token = this.str1 + this.str2 + this.str4;
-      if (this.usuario.cpf !== undefined && this.usuario.cpf.includes('.')) {
-          const cpfV = /\./gi;
-          this.usuario.cpf = this.usuario.cpf.replace(cpfV, '');
-          this.usuario.cpf = this.usuario.cpf.replace('-', '');
-      }
-      if (this.usuario.cpf.length > 10) {
-          this.cadastroService.getValidarCpf(this.usuario.cpf, this.token).subscribe(resultado => {
-              this.retorno = resultado;
-              if (this.retorno.status === '1') {
-                  this.usuario.cpf = this.retorno.data.number_formatted;
-              } else {
-                  this.usuario.cpf = '';
-              }
-          });
+      if (this.usuario.cpf !== '') {
+          if (this.usuario.cpf.includes('.')) {
+              const cpfV = /\./gi;
+              this.usuario.cpf = this.usuario.cpf.replace(cpfV, '');
+              this.usuario.cpf = this.usuario.cpf.replace('-', '');
+          }
+          if (this.usuario.cpf.length > 10) {
+              this.cadastroService.getValidarCpf(this.usuario.cpf, this.token).subscribe(resultado => {
+                  this.retorno = resultado;
+                  if (this.retorno.status === '1') {
+                      this.usuario.cpf = this.retorno.data.number_formatted;
+                  } else {
+                      this.usuario.cpf = '';
+                  }
+              });
+          }
       }
   }
-
 }
--- src/app/cadastro/cadastro.service.ts
+++ src/app/cadastro/cadastro.service.ts
@@ -1,27 +1,41 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Http,
+  Response
+} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { usuarioModel } from './../models/usuarioModel';
-export type EntityResponseType = HttpResponse<usuarioModel>;
+import {
+  HttpClient,
+  HttpResponse
+} from '@angular/common/http';
+import {
+  Observable
+} from 'rxjs/Observable';
+import {
+  usuarioModel
+} from './../models/usuarioModel';
+export type EntityResponseType = HttpResponse < usuarioModel > ;
 @Injectable()
 export class CadastroService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private urlCep = 'https://viacep.com.br/ws';
   private urlCpf = 'http://geradorapp.com/api/v1/cpf/validate'
   private urlPost = 'http://demo4677424.mockable.io/usuarios/';
 
-  salvarUsuario(usuario: usuarioModel): Observable<EntityResponseType> {
-    return this.http.post<usuarioModel>(this.urlPost, usuario,{ observe: 'response' });
+  salvarUsuario(usuario: usuarioModel): Observable < EntityResponseType > {
+      return this.http.post < usuarioModel > (this.urlPost, usuario, {
+          observe: 'response'
+      });
   }
   getCep(cep: string) {
-    return this.http.get(`${this.urlCep}/${cep}/json/`);
+      return this.http.get(`${this.urlCep}/${cep}/json/`);
   }
   getValidarCpf(cpf: string, tk: string) {
-    return this.http.get(`${this.urlCpf}/${cpf}?token=${tk}`);
+      return this.http.get(`${this.urlCpf}/${cpf}?token=${tk}`);
   }
 
 }
